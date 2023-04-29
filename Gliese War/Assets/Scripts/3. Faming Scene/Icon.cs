f2bc2d59430f1a40ac241032e920866c3d6adb6e
hsson0822 <hsson0822@naver.com>
2023-04-30 02:58:27 +0900

    [추가,변경] 미니맵 수정및 아이콘, 무기 번호 이미지, 꽃 위치, 상호작용 이미지

---
+++ Gliese War/Assets/Scripts/3. Faming Scene/Icon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icon : MonoBehaviour
+{
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(90f,0,90f);
+    }
+}
