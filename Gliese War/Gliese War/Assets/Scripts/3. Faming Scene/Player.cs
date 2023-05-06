04a5536567349bf6753eabd73c8f78bd3435f957
hsson0822 <hsson0822@naver.com>
2023-05-06 17:46:05 +0900

    [변경] 몬스터 데미지 수정

--- Gliese War/Gliese War/Assets/Scripts/3. Faming Scene/Player.cs
+++ Gliese War/Gliese War/Assets/Scripts/3. Faming Scene/Player.cs
@@ -591,7 +591,7 @@ public class Player : MonoBehaviour
     {
         if (other.CompareTag("MonsterAttack"))
         {
-            GetDamage(10);
+            GetDamage(2);
             FarmingManager.Instance.HitScreen();
         }
     }
