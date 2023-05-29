0085ac03969d3a95116c50f916033082b0e0f814
hsson0822 <hsson0822@naver.com>
2023-05-29 16:58:42 +0900

    [추가] 로비 씬 디자인, 설명창 기능 및 애니메이션 추가

--- Gliese War/Gliese War/Assets/Scripts/2. Lobby Scene/LobbyManager.cs
+++ Gliese War/Gliese War/Assets/Scripts/2. Lobby Scene/LobbyManager.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using MySql.Data.MySqlClient;
 
 public class LobbyManager : MonoBehaviour
 {
+    private MySqlDataReader reader;
     private string id;
 
     [Header("Lobby")] [SerializeField] private GameObject nickNameText;
@@ -16,20 +19,38 @@ public class LobbyManager : MonoBehaviour
     [SerializeField] private GameObject careerListParent;
     private TextMeshProUGUI[] careerList;
     private Animation careerAnimation;
+
+    [Header("Help")]
+    [SerializeField] private Canvas helpCanvas;
+    [SerializeField] private Sprite[] helpImages;
+    private Animation helpAnimation;
     
+    
+    [Header("Option")]
     [SerializeField] private Canvas optionCanvas;
+    
 
     private void Awake()
     {
-        if (PlayerPrefs.HasKey("ID"))
-            id = PlayerPrefs.GetString("ID");
-        else
-            id = "test1234";
+        id = PlayerPrefs.GetString("ID");
 
-        nickNameText.GetComponent<TextMeshProUGUI>().text = id;
+        if (PlayerPrefs.HasKey("ID"))
+        {
+            reader = MySqlConnector.Instance.doQuery("select * from User where ID = '" + id + "'");
+            reader.Read();
+            string nick = reader[2].ToString();
+            nickNameText.GetComponent<TextMeshProUGUI>().text = nick;
+        }
+        else
+        {
+            //nickNameText.GetComponent<TextMeshProUGUI>().text = id;
+            nickNameText.GetComponent<TextMeshProUGUI>().text = "1234";
+        }
 
         careerList = careerListParent.GetComponentsInChildren<TextMeshProUGUI>();
         careerAnimation = careerCanvas.transform.GetChild(0).GetComponent<Animation>();
+        helpAnimation = helpCanvas.transform.GetChild(0).GetComponent<Animation>();
+        
     }
 
     private void Start()
@@ -47,9 +68,13 @@ public class LobbyManager : MonoBehaviour
         careerCanvas.gameObject.SetActive(true);
         careerAnimation.Play("CareerAnimation");
         
+        //reader = MySqlConnector.Instance.doQuery("select * from Career where ID = '" + id + "'");
+        //reader = MySqlConnector.Instance.doQuery("select * from Career where ID = 'hsson'");
+        //reader.Read();
+
         for (int i = 0; i < careerList.Length; ++i)
         {
-            careerList[i].GetComponent<TextMeshProUGUI>().text = "2";
+            //careerList[i].GetComponent<TextMeshProUGUI>().text = reader[i + 1].ToString();
         }
     }
 
@@ -66,5 +91,23 @@ public class LobbyManager : MonoBehaviour
     public void CloseOptionCanvas()
     {
         optionCanvas.gameObject.SetActive(false);
+    }
+    
+    public void OpenHelp()
+    {
+        helpCanvas.gameObject.SetActive(true);
+        helpAnimation.Play("HelpAnimation");
+
+        HelpButton(0);
+    }
+
+    public void HelpButton(int n)
+    {
+        helpCanvas.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>().sprite = helpImages[n];
+    }
+
+    public void CloseHelp()
+    {
+        helpAnimation.Play("ReturnHelpAnimation");
     }
 }
