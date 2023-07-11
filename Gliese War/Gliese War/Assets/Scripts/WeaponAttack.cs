1360a91349e54680cf0d346182c316938c72756f
hsson0822 <hsson0822@naver.com>
2023-07-11 12:07:52 +0900

    [변경] 필요없는 코드 삭제

--- Gliese War/Gliese War/Assets/Scripts/WeaponAttack.cs
+++ Gliese War/Gliese War/Assets/Scripts/WeaponAttack.cs
@@ -15,103 +15,51 @@ public class WeaponAttack : MonoBehaviour
 
     public void AttackStart()
     {
-        Debug.Log("A");
-        if (SceneManager.GetActiveScene().buildIndex == 2)
+
+        if (CPlayer.Instance.weaponNow == 1)
         {
-            Debug.Log("B");
-            if (CPlayer.Instance.weaponNow == 1)
+            switch (CPlayer.Instance.weapon1.item.weaponType)
             {
-                switch (CPlayer.Instance.weapon1.item.weaponType)
-                {
-                    case Item.WeaponType.Hammer:
-                        TurnOnHandHammer();
-                        StartCoroutine(TurnOffHandHammer());
-                        break;
-                
-                    case Item.WeaponType.Sword:
-                        TurnOnHandSword();
-                        StartCoroutine(TurnOffHandSword());
-                        break;
-                
-                    case Item.WeaponType.Spear:
-                        TurnOnHandSpear();
-                        StartCoroutine(TurnOffHandSpear());
-                        break;
-                }
-            }
-            else if (CPlayer.Instance.weaponNow == 2)
-            {
+                case Item.WeaponType.Hammer:
+                    TurnOnHandHammer();
+                    StartCoroutine(TurnOffHandHammer());
+                    break;
 
-                switch (CPlayer.Instance.weapon2.item.weaponType)
-                {
-                    case Item.WeaponType.Hammer:
-                        TurnOnHandHammer();
-                        StartCoroutine(TurnOffHandHammer());
-                        break;
-                
-                    case Item.WeaponType.Sword:
-                        TurnOnHandSword();
-                        StartCoroutine(TurnOffHandSword());
-                        break;
-                
-                    case Item.WeaponType.Spear:
-                        TurnOnHandSpear();
-                        StartCoroutine(TurnOffHandSpear());
-                        break;
-                }
-            }
+                case Item.WeaponType.Sword:
+                    TurnOnHandSword();
+                    StartCoroutine(TurnOffHandSword());
+                    break;
 
-            CPlayer.Instance.isAttack = true;
+                case Item.WeaponType.Spear:
+                    TurnOnHandSpear();
+                    StartCoroutine(TurnOffHandSpear());
+                    break;
+            }
         }
-        else if (SceneManager.GetActiveScene().buildIndex == 3)
+        else if (CPlayer.Instance.weaponNow == 2)
         {
-            Debug.Log("C");
-            if (BattlePlayer.instance.weaponNow == 1)
-            {
-                switch (BattlePlayer.instance.weapon1.item.weaponType)
-                {
-                    case Item.WeaponType.Hammer:
-                        TurnOnHandHammer();
-                        StartCoroutine(TurnOffHandHammerB());
-                        break;
-                
-                    case Item.WeaponType.Sword:
-                        TurnOnHandSword();
-                        StartCoroutine(TurnOffHandSwordB());
-                        break;
-                
-                    case Item.WeaponType.Spear:
-                        TurnOnHandSpear();
-                        StartCoroutine(TurnOffHandSpearB());
-                        break;
-                }
-            }
-            else if (BattlePlayer.instance.weaponNow == 2)
-            {
 
-                switch (BattlePlayer.instance.weapon2.item.weaponType)
-                {
-                    case Item.WeaponType.Hammer:
-                        TurnOnHandHammer();
-                        StartCoroutine(TurnOffHandHammerB());
-                        break;
-                
-                    case Item.WeaponType.Sword:
-                        TurnOnHandSword();
-                        StartCoroutine(TurnOffHandSwordB());
-                        break;
-                
-                    case Item.WeaponType.Spear:
-                        TurnOnHandSpear();
-                        StartCoroutine(TurnOffHandSpearB());
-                        break;
-                }
-            }
+            switch (CPlayer.Instance.weapon2.item.weaponType)
+            {
+                case Item.WeaponType.Hammer:
+                    TurnOnHandHammer();
+                    StartCoroutine(TurnOffHandHammer());
+                    break;
 
-            BattlePlayer.instance.isAttack = true;
+                case Item.WeaponType.Sword:
+                    TurnOnHandSword();
+                    StartCoroutine(TurnOffHandSword());
+                    break;
+
+                case Item.WeaponType.Spear:
+                    TurnOnHandSpear();
+                    StartCoroutine(TurnOffHandSpear());
+                    break;
+            }
         }
-        
-        
+
+        CPlayer.Instance.isAttack = true;
+
     }
 
     public void TurnOnHandHammer()
@@ -168,72 +116,6 @@ public class WeaponAttack : MonoBehaviour
     }
     
     IEnumerator TurnOffHandSword()
-    {
-        yield return new WaitForSeconds(0.9f);
-        
-        col = CPlayer.Instance.handR.transform.GetChild(2).GetComponent<MeshCollider>();
-        col.enabled = false;
-        
-        CPlayer.Instance.handR.transform.GetChild(2).gameObject.SetActive(false);
-        CPlayer.Instance.back.transform.GetChild(2).gameObject.SetActive(true);
-        
-        CPlayer.Instance.isAttack = false;
-    }
-    
-    public void TurnOnHandHammerB()
-    {
-        CPlayer.Instance.handR.transform.GetChild(0).gameObject.SetActive(true);
-        CPlayer.Instance.back.transform.GetChild(0).gameObject.SetActive(false);
-        
-        col = CPlayer.Instance.handR.transform.GetChild(0).GetComponent<MeshCollider>();
-        col.enabled = true;
-    }
-    
-    public void TurnOnHandSpearB()
-    {
-        CPlayer.Instance.handR.transform.GetChild(1).gameObject.SetActive(true);
-        CPlayer.Instance.back.transform.GetChild(1).gameObject.SetActive(false);
-        
-        col = CPlayer.Instance.handR.transform.GetChild(1).GetComponent<MeshCollider>();
-        col.enabled = true;
-    }
-    
-    public void TurnOnHandSwordB()
-    {
-        CPlayer.Instance.handR.transform.GetChild(2).gameObject.SetActive(true);
-        CPlayer.Instance.back.transform.GetChild(2).gameObject.SetActive(false);
-        
-        col = CPlayer.Instance.handR.transform.GetChild(2).GetComponent<MeshCollider>();
-        col.enabled = true;
-    }
-
-    IEnumerator TurnOffHandHammerB()
-    {
-        yield return new WaitForSeconds(0.5f);
-        
-        col = CPlayer.Instance.handR.transform.GetChild(0).GetComponent<MeshCollider>();
-        col.enabled = false;
-        
-        CPlayer.Instance.handR.transform.GetChild(0).gameObject.SetActive(false);
-        CPlayer.Instance.back.transform.GetChild(0).gameObject.SetActive(true);
-        
-        CPlayer.Instance.isAttack = false;
-    }
-    
-    IEnumerator TurnOffHandSpearB()
-    {
-        yield return new WaitForSeconds(0.6f);
-        
-        col = CPlayer.Instance.handR.transform.GetChild(1).GetComponent<MeshCollider>();
-        col.enabled = false;
-        
-        CPlayer.Instance.handR.transform.GetChild(1).gameObject.SetActive(false);
-        CPlayer.Instance.back.transform.GetChild(1).gameObject.SetActive(true);
-        
-        CPlayer.Instance.isAttack = false;
-    }
-    
-    IEnumerator TurnOffHandSwordB()
     {
         yield return new WaitForSeconds(0.9f);
         
