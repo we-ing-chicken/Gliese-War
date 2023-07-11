2909812a8191bc2905204b565df71384d017abe5
hsson0822 <hsson0822@naver.com>
2023-07-11 10:45:59 +0900

    [변경] 배틀씬, 배틀매니저 합치기

--- Gliese War/Gliese War/Assets/Scripts/4. Battle Scene/BattleManager.cs
+++ Gliese War/Gliese War/Assets/Scripts/4. Battle Scene/BattleManager.cs
@@ -6,6 +6,15 @@ public class BattleManager : MonoBehaviour
 {
     private static BattleManager _instance;
     public List<int> charNums = new List<int>();
+    
+    [Header("Canvas")] public GameObject invenCanvas;
+    public Canvas pauseCanvas;
+    public Canvas fadeCanvas;
+    public Canvas hitCanvas;
+
+    public Item[] sword;
+    public Item[] spear;
+    public Item[] hammer;
 
     public static BattleManager Instance
     {
@@ -47,5 +56,44 @@ public class BattleManager : MonoBehaviour
                 
         }
 
+    }
+    
+    private void SwitchCanvasActive(Canvas temp)
+    {
+        if (temp.gameObject.activeSelf)
+            temp.gameObject.SetActive(false);
+        else
+            temp.gameObject.SetActive(true);
+    }
+    
+    private void SwitchCanvasActive(GameObject temp)
+    {
+        if (temp.gameObject.activeSelf)
+            temp.gameObject.SetActive(false);
+        else
+            temp.gameObject.SetActive(true);
+    }
+    private void SwitchGameObjectActive(GameObject temp)
+    {
+        if (temp.gameObject.activeSelf)
+            temp.gameObject.SetActive(false);
+        else
+            temp.gameObject.SetActive(true);
+    }
+    
+    public void HitScreen()
+    {
+        StartCoroutine(HitCoroutine());
+    }
+
+    IEnumerator HitCoroutine()
+    {
+        for (int i = 0; i < 6; ++i)
+        {
+            SwitchCanvasActive(hitCanvas);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return null;
     }
 }
