2004f21eaff45253abeee8e9f9e2e05d14ac9844
primrose <bigjht34@gmail.com>
2023-07-13 17:06:42 +0900

    [변경] 이동 동기화 로직

--- Gliese War/Gliese War/Assets/Scripts/4. Battle Scene/BattlePlayer.cs
+++ Gliese War/Gliese War/Assets/Scripts/4. Battle Scene/BattlePlayer.cs
@@ -312,7 +312,7 @@ public class BattlePlayer : LivingEntity, IPunObservable
         else
         {
             //ismove = (remoteDir.x > 0 || remoteDir.z > 0);
-            transform.position = remotePos;
+            transform.position += (remotePos - transform.position).normalized * moveSpeed * Time.deltaTime;
 
             remoteRot = Quaternion.Euler(0, MouseX, 0);
             transform.rotation = remoteRot;
