177b6ba9ab2e296f35ab2dbd9e8274b9b473965f
hsson0822 <hsson0822@naver.com>
2023-07-26 02:12:57 +0900

    [조정] 장비 1차 밸런스 조절

--- Gliese War/Gliese War/Assets/Scripts/3. Faming Scene/Inventory/Slot.cs
+++ Gliese War/Gliese War/Assets/Scripts/3. Faming Scene/Inventory/Slot.cs
@@ -166,26 +166,220 @@ public class Slot : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDra
         switch (realItem.item.itemCategory)
         {
             case Item.ItemCategory.Helmet:
-                realItem.stat.defensePower = GetRankStat(realItem.item);
-                realItem.stat.health = GetRankStat(realItem.item);
+                //realItem.stat.defensePower = GetRankStat(realItem.item);
+                //realItem.stat.health = GetRankStat(realItem.item);
+                SetHelmetStat(realItem);
                 break;
 
             case Item.ItemCategory.Armor:
-                realItem.stat.defensePower = GetRankStat(realItem.item);
-                realItem.stat.health = GetRankStat(realItem.item);
+                //realItem.stat.defensePower = GetRankStat(realItem.item);
+                //realItem.stat.health = GetRankStat(realItem.item);
+                SetArmorStat(realItem);
                 break;
 
             case Item.ItemCategory.Shoes:
-                realItem.stat.defensePower = GetRankStat(realItem.item);
-                realItem.stat.moveSpeed = GetRankStat(realItem.item);
+                //realItem.stat.defensePower = GetRankStat(realItem.item);
+                //realItem.stat.moveSpeed = GetRankStat(realItem.item);
+                SetShoeStat(realItem);
                 break;
 
             case Item.ItemCategory.Weapon:
-                realItem.stat.attackPower = GetRankStat(realItem.item);
+                //realItem.stat.attackPower = GetRankStat(realItem.item);
+                SetWeaponStat(realItem);
+                break;
+        }
+    }
+
+    private void SetHelmetStat(RealItem realItem)
+    {
+        switch (realItem.item.itemRank)
+        {
+            case Item.ItemRank.Normal:
+                realItem.stat.defensePower = Random.Range(3, 8);
+                realItem.stat.health = Random.Range(10, 18);
+                break;
+
+            case Item.ItemRank.Rare:
+                realItem.stat.defensePower = Random.Range(8, 13);
+                realItem.stat.health = Random.Range(18, 26);
+                break;
+            
+            case Item.ItemRank.Epic:
+                realItem.stat.defensePower = Random.Range(13, 18);
+                realItem.stat.health = Random.Range(26, 34);
+                break;
+            
+            case Item.ItemRank.Unique:
+                realItem.stat.defensePower = Random.Range(18, 23);
+                realItem.stat.health = Random.Range(34, 42);
+                break;
+            
+            case Item.ItemRank.Legendary:
+                realItem.stat.defensePower = Random.Range(23, 28);
+                realItem.stat.health = Random.Range(42, 51);
                 break;
         }
     }
     
+    private void SetArmorStat(RealItem realItem)
+    {
+        switch (realItem.item.itemRank)
+        {
+            case Item.ItemRank.Normal:
+                realItem.stat.defensePower = Random.Range(2, 6);
+                realItem.stat.health = Random.Range(10, 18);
+                break;
+
+            case Item.ItemRank.Rare:
+                realItem.stat.defensePower = Random.Range(6, 10);
+                realItem.stat.health = Random.Range(18, 26);
+                break;
+            
+            case Item.ItemRank.Epic:
+                realItem.stat.defensePower = Random.Range(10, 14);
+                realItem.stat.health = Random.Range(26, 34);
+                break;
+            
+            case Item.ItemRank.Unique:
+                realItem.stat.defensePower = Random.Range(14, 18);
+                realItem.stat.health = Random.Range(34, 42);
+                break;
+            
+            case Item.ItemRank.Legendary:
+                realItem.stat.defensePower = Random.Range(18, 22);
+                realItem.stat.health = Random.Range(42, 51);
+                break;
+        }
+    }
+    
+    private void SetShoeStat(RealItem realItem)
+    {
+        switch (realItem.item.itemRank)
+        {
+            case Item.ItemRank.Normal:
+                realItem.stat.defensePower = Random.Range(1, 4);
+                realItem.stat.moveSpeed = 1;
+                break;
+
+            case Item.ItemRank.Rare:
+                realItem.stat.defensePower = Random.Range(4, 7);
+                realItem.stat.moveSpeed = 2;
+                break;
+            
+            case Item.ItemRank.Epic:
+                realItem.stat.defensePower = Random.Range(7, 10);
+                realItem.stat.moveSpeed = 3;
+                break;
+            
+            case Item.ItemRank.Unique:
+                realItem.stat.defensePower = Random.Range(10, 13);
+                realItem.stat.moveSpeed = 4;
+                break;
+            
+            case Item.ItemRank.Legendary:
+                realItem.stat.defensePower = Random.Range(13, 16);
+                realItem.stat.moveSpeed = 5;
+                break;
+        }
+    }
+
+    private void SetWeaponStat(RealItem realItem)
+    {
+        switch (realItem.item.weaponType)
+        {
+            case Item.WeaponType.Hammer:
+                SetHammerStat(realItem);
+                break;
+            
+            case Item.WeaponType.Spear:
+                SetSpearStat(realItem);
+                break;
+            
+            case Item.WeaponType.Sword:
+                SetSwordStat(realItem);
+                break;
+        }
+    }
+    
+    private void SetHammerStat(RealItem realItem)
+    {
+        switch (realItem.item.itemRank)
+        {
+            case Item.ItemRank.Normal:
+                realItem.stat.attackPower = Random.Range(3, 9);
+                break;
+
+            case Item.ItemRank.Rare:
+                realItem.stat.attackPower = Random.Range(9, 15);
+                break;
+            
+            case Item.ItemRank.Epic:
+                realItem.stat.attackPower = Random.Range(15, 21);
+                break;
+            
+            case Item.ItemRank.Unique:
+                realItem.stat.attackPower = Random.Range(21, 26);
+                break;
+            
+            case Item.ItemRank.Legendary:
+                realItem.stat.attackPower = Random.Range(26, 34);
+                break;
+        }
+    }
+    
+    private void SetSpearStat(RealItem realItem)
+    {
+        switch (realItem.item.itemRank)
+        {
+            case Item.ItemRank.Normal:
+                realItem.stat.attackPower = Random.Range(1, 5);
+                break;
+
+            case Item.ItemRank.Rare:
+                realItem.stat.attackPower = Random.Range(5, 9);
+                break;
+            
+            case Item.ItemRank.Epic:
+                realItem.stat.attackPower = Random.Range(9, 12);
+                break;
+            
+            case Item.ItemRank.Unique:
+                realItem.stat.attackPower = Random.Range(12, 16);
+                break;
+            
+            case Item.ItemRank.Legendary:
+                realItem.stat.attackPower = Random.Range(16, 21);
+                break;
+        }
+    }
+    
+    private void SetSwordStat(RealItem realItem)
+    {
+        switch (realItem.item.itemRank)
+        {
+            case Item.ItemRank.Normal:
+                realItem.stat.attackPower = Random.Range(2, 7);
+                break;
+
+            case Item.ItemRank.Rare:
+                realItem.stat.attackPower = Random.Range(7, 12);
+                break;
+            
+            case Item.ItemRank.Epic:
+                realItem.stat.attackPower = Random.Range(12, 17);
+                break;
+            
+            case Item.ItemRank.Unique:
+                realItem.stat.attackPower = Random.Range(17, 22);
+                break;
+            
+            case Item.ItemRank.Legendary:
+                realItem.stat.attackPower = Random.Range(22, 27);
+                break;
+        }
+    }
+
+
     private int GetRankStat(Item item)
     {
         int power = 0;
