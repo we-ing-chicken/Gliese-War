638d5a147beb2028732e9dcbe0ea5081b50a6b7a
hsson0822 <hsson0822@naver.com>
2023-07-27 19:24:43 +0900

    [추가] 회복 아이템 회전, 위아래 움직이기 추가

---
+++ Gliese War/Gliese War/Assets/Scripts/4. Battle Scene/ItemMoving.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ItemMoving : MonoBehaviour
+{
+    float initY;
+    float dis;
+    float turningPoint;
+    float rotSpeed = 100f;
+    bool isUD = true;
+
+    private void Start()
+    {
+        initY = transform.position.y;
+        turningPoint = initY - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, rotSpeed * Time.deltaTime, 0));
+        
+        if (transform.position.y >= initY)
+        {
+            isUD = false;
+        }
+        else if (transform.position.y <= turningPoint)
+        {
+            isUD = true;
+        }
+
+        if (isUD)
+        {
+            
+            transform.position = transform.position + new Vector3(0, 1, 0) * Time.deltaTime;
+        }
+        else
+        {
+            
+            transform.position = transform.position + new Vector3(0, -1, 0) * Time.deltaTime;
+        }
+
+    }
+}
